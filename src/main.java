9706ceb2181e73e6411653f326447a082b7a05d0
Daewon Kim <eodnjs2998@gmail.com>
2012-12-01 23:08:48 +0900

    Test

--- src/main.java
+++ src/main.java
@@ -1,12 +1,11 @@
 
-public class Main {
+public class main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("abc");
 	}
 
 }
